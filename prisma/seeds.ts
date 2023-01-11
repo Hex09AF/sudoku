dd62e0b52aa0fdde943231aff56993e510990ddf
Hex09AF <lsqhuy@gmail.com>
2023-01-11 17:53:15 +0700

    Hash more user

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -10,6 +10,13 @@ async function seed() {
         "$2a$10$J3KUUA1ZjkgxmjIhOpztYuMfy9ovEiRBoRz4pQgExIq/8wJWWbluW",
     },
   });
+  const kody2 = await db.user.create({
+    data: {
+      username: "kody2",
+      passwordHash:
+        "$2a$10$4RIfbFVuMqM4jkvZmrd7CefU4NmBNqnllHU1D9QMG1RoAaPyqoVFy",
+    },
+  });
 }
 
 seed();
