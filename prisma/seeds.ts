dfcb81ccc1656cb58dbcb33cb84b5530d8328d32
Hex09AF <lsqhuy@gmail.com>
2023-06-01 16:46:47 +0700

    Fix: seed upsert

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -1,38 +1,57 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
-
 async function seed() {
-  const kody = await db.user.create({
-    data: {
+  const kody = await db.user.upsert({
+    where: {
+      username: "kody",
+    },
+    update: {},
+    create: {
       username: "kody",
       passwordHash:
         "$2a$10$J3KUUA1ZjkgxmjIhOpztYuMfy9ovEiRBoRz4pQgExIq/8wJWWbluW",
     },
   });
-  const kody2 = await db.user.create({
-    data: {
+  const kody2 = await db.user.upsert({
+    where: {
+      username: "kody2",
+    },
+    update: {},
+    create: {
       username: "kody2",
       passwordHash:
         "$2a$10$4RIfbFVuMqM4jkvZmrd7CefU4NmBNqnllHU1D9QMG1RoAaPyqoVFy",
     },
   });
-  const kody3 = await db.user.create({
-    data: {
+  const kody3 = await db.user.upsert({
+    where: {
+      username: "kody3",
+    },
+    update: {},
+    create: {
       username: "kody3",
       passwordHash:
         "$2a$10$4RIfbFVuMqM4jkvZmrd7CefU4NmBNqnllHU1D9QMG1RoAaPyqoVFy",
     },
   });
-  const kody4 = await db.user.create({
-    data: {
+  const kody4 = await db.user.upsert({
+    where: {
+      username: "kody4",
+    },
+    update: {},
+    create: {
       username: "kody4",
       passwordHash:
         "$2a$10$4RIfbFVuMqM4jkvZmrd7CefU4NmBNqnllHU1D9QMG1RoAaPyqoVFy",
     },
   });
-  const kody5 = await db.user.create({
-    data: {
+  const kody5 = await db.user.upsert({
+    where: {
+      username: "kody5",
+    },
+    update: {},
+    create: {
       username: "kody5",
       passwordHash:
         "$2a$10$4RIfbFVuMqM4jkvZmrd7CefU4NmBNqnllHU1D9QMG1RoAaPyqoVFy",
